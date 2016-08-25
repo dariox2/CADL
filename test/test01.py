35a6f241713fe435682f284262abd43039aae62f
dariox2 <dariox2@github.com>
2016-08-25 08:26:18 -0300

    mix 2 images: how to piss a NN off

--- test/test01.py
+++ test/test01.py
@@ -14,6 +14,19 @@ import tensorflow as tf
 from libs import gif
 import IPython.display as ipyd
 
+
+n_iterations = 100
+LAYERSIZE=64
+NHIDLAYERS=5
+
+tamimg=64
+#filenames=["barvert.png", "barvert.png"]
+#filenames=["barhoriz.png", "barhoriz.png"]
+filenames=["barhoriz.png", "barvert.png"]
+
+gif_frames=50
+plot_step=1
+
 np.set_printoptions(threshold=np.inf) # display FULL array (infinite)
 
 # (from utils.py)
@@ -85,12 +98,10 @@ plt.ion()
 #plt.show()
 #plt.pause(2)
 
-filenames=["barvert.png", "barvert.png"]
 origimg = [plt.imread(fname)[..., :3] for fname in filenames]
 
-
-imrsz0=imresize(origimg[0], (20,20))
-imrsz1=imresize(origimg[1], (20,20))
+imrsz0=imresize(origimg[0], (tamimg,tamimg))
+imrsz1=imresize(origimg[1], (tamimg,tamimg))
 #print("imrsz0=",imrsz0)
 #print("imrsz1=",imrsz1)
 scaledimg = [imrsz0, imrsz1]
@@ -113,7 +124,7 @@ if plotgraph:
 xs0, ys0 = split_image(scaledimg[0])
 xs1, ys1 = split_image(scaledimg[1])
 
-print(xs0.__class__)
+#print(xs0.__class__)
 
 xs=np.asarray([xs0, xs1])
 ys=np.asarray([ys0, ys1])
@@ -125,15 +136,15 @@ ys=np.asarray([ys0, ys1])
 #print(xs.__class__)
 #print(xs.shape)
 
-print(xs.shape)
+#print(xs.shape)
 
 xs=xs.reshape(xs.shape[0]*xs.shape[1], 2)
 ys=ys.reshape(ys.shape[0]*ys.shape[1], 3)
 
 print("xs, ys shape:" , xs.shape, ys.shape)
 
-print("xs=",xs)
-print("ys=",ys)
+#print("xs=",xs)
+#print("ys=",ys)
 
 #print("============ ys:")
 #print(ys)
@@ -157,8 +168,6 @@ tf.reset_default_graph()
 X = tf.placeholder(tf.float32, shape=[None, 2], name='X')
 Y = tf.placeholder(tf.float32, shape=[None, 3], name='Y')
 
-LAYERSIZE=64
-NHIDLAYERS=5
 n_neurons = [2, LAYERSIZE, LAYERSIZE, LAYERSIZE, LAYERSIZE, LAYERSIZE, LAYERSIZE,  3]
 
 print("LAYERSIZE=",LAYERSIZE)
@@ -190,8 +199,6 @@ costtot = tf.reduce_mean(sum_errorred)
 assert(costtot.get_shape().as_list() == [])
 
 myoptimizer =tf.train.AdamOptimizer(0.001).minimize(costtot)
-n_iterations = 20
-batch_size = 10
 
 sess = tf.Session()
 
@@ -199,10 +206,10 @@ sess.run(tf.initialize_all_variables())
 
 gifimgs = []
 costs = []
-gif_step = max(n_iterations // 20, 1)
+gif_step = max(n_iterations // gif_frames, 1)
 print("gif_step: ", gif_step)
 step_i = 0
-
+batch_size = int(np.sqrt(len(xs)))
 for it_i in range(n_iterations):
 
     print("iteration: ", it_i, end="", flush=True);
@@ -233,18 +240,15 @@ for it_i in range(n_iterations):
     #print("  cost: ", training_cost / n_batches);
     print("  cost: ", training_cost);
 
-    if (it_i + 1) % gif_step == 0:
-        #costs.append(training_cost / n_batches)
-        costs.append(training_cost)
+    if (it_i + 1) % gif_step == 0 or (it_i + 1) % plot_step == 0:
         idxs_j=range(len(xs)//2)
         ys_pred = Y_pred.eval(feed_dict={X: xs[idxs_j]}, session=sess)
-        #print( "ys_pred shape: ", ys_pred.shape)
-        #print("============ ys_pred:")
-        #print(np.floor(ys_pred))
+        plotimg = np.clip(np.array(ys_pred.reshape(scaledimg[0].shape))*255, 0, CLIPVALUE).astype(np.uint8)
+    if (it_i + 1) % gif_step == 0:
+        gifimgs.append(plotimg)
+    if (it_i + 1) % plot_step == 0:
+        costs.append(training_cost)
         if plotgraph:
-          plotimg = np.clip(np.array(ys_pred.reshape(scaledimg[0].shape))*255, 0, CLIPVALUE).astype(np.uint8)
-          gifimgs.append(plotimg)
-
           plt.imshow(plotimg)
           plt.title('Iteration {}'.format(it_i))
           plt.show()
@@ -256,7 +260,7 @@ if plotgraph:
   # Save the images as a GIF
   _ = gif.build_gif(gifimgs, saveto='test01_single.gif', show_gif=False)
 
-  plt.imsave(fname='test01_predicted.png', arr=plotimg)
+  plt.imsave(fname='test01_predicted.png', arr=plotimg, interval=0.3)
 
   plt.pause(5)
   plt.close()
