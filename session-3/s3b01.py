a7a1f50dd5b1ab1f7ddce6d212e6788e266b6887
dariox2 <dariox@gmail.com>
2016-09-16 22:26:04 -0300

    s3b01 test 180 ep, 6050s

--- session-3/s3b01.py
+++ session-3/s3b01.py
@@ -38,8 +38,8 @@ TID=datetime.date.today().strftime("%Y%m%d")+"_"+datetime.datetime.now().time().
 print("Loading pictures...")
 # See how this works w/ Celeb Images or try your own dataset instead:
 #import os
-dirname = "../session-1/labdogs"
-#dirname = "../../myrandompictures"
+#dirname = "../session-1/labdogs"
+dirname = "../../myrandompictures"
 QNT=100
 filenames = [os.path.join(dirname, fname) for fname in os.listdir(dirname)]
 filenames = filenames[:QNT]
@@ -274,8 +274,8 @@ sess.run(tf.initialize_all_variables())
 
 # Some parameters for training
 batch_size = QNT
-n_epochs = 31
-step = 1
+n_epochs = 180
+step = 6
 
 # We'll try to reconstruct the same first 100 images and show how
 # The network does over the course of training.
